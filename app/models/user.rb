425a630841c7e0a5a5aa0157396177f84008cef7
Scott Luptowski <scottluptowski@gmail.com>
2013-12-06 11:32:57 -0500

    refactor progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+  include Formatter::UserFormatter
+  
   has_many :commits
   has_many :repos, :through => :commits
 
   def self.commit_leader
     User.order("commit_count DESC").detect {|u| u.commit_count }
   end
-
 end
