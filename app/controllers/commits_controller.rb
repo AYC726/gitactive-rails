9d8a0ca57272d6685ca2a0082c0283f337afb6cc
Scott Luptowski <scottluptowski@gmail.com>
2013-12-06 11:47:12 -0500

    better controller

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -3,19 +3,18 @@ class CommitsController < ApplicationController
   def index
     @commits = Commit.recent_commits(3)
     @leader = User.commit_leader
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => json_hash }
+    end
   end
 
-  def json
-    @commits = Commit.recent_commits(3)
-    @leader = User.commit_leader
-
-    json_hash = {
+  private
+  def json_hash
+    {
       :commits => @commits.map(&:json_format),
       :leader => @leader.json_format
     }
-
-    respond_to do |format|
-      format.json { render :json => json_hash }
-    end
   end
 end
